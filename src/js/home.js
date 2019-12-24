cd4f4bc0ae420e82c9e55b86335ff63efec97442
EstebanMeza <emeza06@misena.edu.co>
2019-12-24 13:57:47 -0500

    mitad del curso

--- src/js/home.js
+++ src/js/home.js
@@ -1,70 +1,241 @@
-// console.log('hola mundo!');
-// const noCambia = "Leonidas";
+// ............0
 
-// let cambia = "@LeonidasEsteban"
+(async function load() {
+ async function getData(url) {
+    let response= await fetch(url)
+  let data = await response.json()
+  return data;
+ }
+ let $form=document.getElementById('form');
+ $form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+ })
+ let actionList=await getData('https://yts.lt/api/v2/list_movies.json?genre=action');
+ let dramaList=await getData('https://yts.lt/api/v2/list_movies.json?genre=drama    ');
+ let animationList=await getData('https://yts.lt/api/v2/list_movies.json?genre=animation');
+ let $action=document.querySelector('#action');
+ let $drama=document.getElementById('drama');
+ let $animation=document.getElementById('animation');
 
-// function cambiarNombre(nuevoNombre) {
-//   cambia = nuevoNombre
+ let HTMLString=``;
+let movieElement;
+ function videoItemTemplate(movie) {
+  HTMLString=``;
+  for (i=0;i<movie.data.movies.length;i++){
+    // =videoItemTemplate(actionList.data.movies[0]);
+    HTMLString+=`
+    <div class="">
+      <div class="featuring-image">
+        <img src="${movie.data.movies[i].medium_cover_image}" width="130" height="160" alt="">
+      </div>
+      <div class="featuring-content">
+        <p class="featuring-title">${movie.data.movies[i].title}</p>
+        <p class="featuring-album"></p>
+      </div>
+    </div>
+    `}
+  return (HTMLString)
+
+}
+function addEventClick($element) {
+  $element.addEventListener('click',function(){
+    alert('click');
+  });
+  // $element.addEventListener('click',function(){
+  // alert('click');
+  // })
+}
+function createTemplate(HTMLString,container) {
+  container.children[0].remove();
+  // img=document.getElementById('img_').value;
+  // img.children[0].remuve(); 
+  let html= document.implementation.createHTMLDocument();
+  html.body.innerHTML=HTMLString;
+// debugger
+
+for(i=0;i<html.body.children.length;i++){
+  movieElement=html.body.children[0];
+  container.append(movieElement);
+  addEventClick(movieElement);
+  // $action.append(html.body.children[1]);
+  // $action.append(html.body.children[2]);
+  // $action.append(html.body.children[3]);
+
+}
+}
+
+
+function renderizar(lista,tipo) {
+  
+  videoItemTemplate(lista)
+  createTemplate(HTMLString,tipo);
+  
+}
+renderizar(actionList,$action)
+renderizar(dramaList,$drama)
+renderizar(animationList,$animation)
+// debugger;
+//   console.log(data)  // await
+// let $modal=document.getElementById('modal');
+// let $overlay=document.getElementById('overlay');
+// // let $hideModal=Document.getElementById('hide-modal');
+// let $animationContainer=document.getElementById('animation');
+// let $featuringContainer=document.getElementById('featuring');
+// let $featuringContainer=document.getElementById('animation');
+// let $home=document.getElementById('home');
+// let $modalTite=$modal.querySelector('h1');
+// let $modalImage=$modal.querySelector('img');
+// let $modalDescription=$modal.querySelector('p');
+
+// function videoItemTemplate(src,image) {
+//   return (
+//     `
+//       <div class="featuring">
+//         <div class="featuring-image">
+//           <img src="${src}" width="70" height="100" alt="">
+//         </div>
+//         <div class="featuring-content">
+//           <p class="featuring-title">${image}</p>
+//           <p class="featuring-album"></p>
+//         </div>
+//       </div>
+//       `
+//  )
 // }
+// // console.log(videoItemTemplate('src/images/platzi-video.png','platzi'))
 
-// const getUserAll = new Promise(function(todoBien, todoMal) {
-//   // llamar a un api
-//   setTimeout(function() {
-//     // luego de 3 segundos
-//     todoBien('se acabó el tiempo');
-//   }, 5000)
-// })
 
-// const getUser = new Promise(function(todoBien, todoMal) {
-//   // llamar a un api
-//   setTimeout(function() {
-//     // luego de 3 segundos
-//     todoBien('se acabó el tiempo 3');
-//   }, 3000)
-// })
-
-// // getUser
-// //   .then(function() {
-// //     console.log('todo está bien en la vida')
-// //   })
-// //   .catch(function(message) {
-// //     console.log(message)
-// //   })
-
-// Promise.race([
-//   getUser,
-//   getUserAll,
-// ])
-// .then(function(message) {
-//   console.log(message);
-// })
-// .catch(function(message) {
-//   console.log(message)
-// })
+})()
 
 
 
-// $.ajax('https://randomuser.me/api/sdfdsfdsfs', {
-//   method: 'GET',
-//   success: function(data) {
-//     console.log(data)
-//   },
-//   error: function(error) {
-//     console.log(error)
-//   }
-// })
 
-// fetch('https://randomuser.me/api/dsfdsfsd')
-//   .then(function (response) {
-//     // console.log(response)
-//     return response.json()
-//   })
-//   .then(function (user) {
-//     console.log('user', user.results[0].name.first)
-//   })
-//   .catch(function() {
-//     console.log('algo falló')
-//   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // (async function load() {
